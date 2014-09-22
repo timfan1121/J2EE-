396f2991e856862ff878f321e0217454055f14ca
Tim <timfan1121@gmail.com>
2014-09-23 01:38:33 +0800

    spring + javaMail + velocity模板

---
+++ spring_javaMail_Velocity.mk
@@ -0,0 +1,3 @@
+* [【转】spring+velocity+javamail发送模板邮件 ](http://www.blogjava.net/fanyingjie/archive/2013/08/29/403429.html) 
+* [Spring邮件服务：Maven + Spring SMTP Mail](http://www.blogjava.net/fanyingjie/archive/2013/08/29/403429.html)
+* [Spring mail 施用多个账号发送带有附件的HTML邮件 ](http://www.myexception.cn/ai/924498.html)
